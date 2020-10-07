38e6be2ea78af279b2035959cf961ebc24265ff1
sssodahae <qwer6897@nate.com>
2020-10-07 11:56:37 +0900

    201007 Dahae : comment model, follow

--- note/models.py
+++ note/models.py
@@ -34,9 +34,10 @@ class Comment(models.Model):
     comment_id = models.AutoField(primary_key=True)
     note_id = models.ForeignKey(Note, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.PROTECT)
+    user_img = models.FileField(default="user.svg")
     # note_id = models.IntegerField(default=0)
     # user_id = models.IntegerField(default=0)
-    user_name = models.CharField(max_length=200, default="")
+    user_name = models.CharField(max_length=200, default="user.svg")
     comment_contents = models.TextField()
     # 글자수 제한 두는 게 좋을 듯
     comment_date = models.DateTimeField(auto_now_add=True, blank=True)
